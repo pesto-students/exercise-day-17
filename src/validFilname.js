e2df7b81dd3758cabdfd560dd6bce382d1e89492
monisahmad <monis.ahmad42@gmail.com>
2018-06-27 00:28:42 +0530

    ValidFilename

--- src/validFilname.js
+++ src/validFilname.js
@@ -2,4 +2,21 @@
   Check if filename is valid. Check out the test cases for `validFilename` for all cases.
 */
 
-module.exports = () => {};
+const validFilename = (fileName) => {
+  if (fileName.includes('/')) {
+    return false;
+  }
+  if (fileName.length === 0 || fileName.length > 255) {
+    return false;
+  }
+  if (fileName.includes('<') || fileName.includes('>') || fileName.includes('|')) {
+    return false;
+  }
+  if (fileName === 'con' || fileName === 'aux' || fileName === 'com1'
+  || fileName === 'lpt1' || fileName === '.' || fileName === '..') {
+    return false;
+  }
+  return true;
+};
+
+module.exports = validFilename;
