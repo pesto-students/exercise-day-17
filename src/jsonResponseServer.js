d88b96b22aa64aaccc49724e131e0a9185a4d1d0
Nikhil Arora <narora200@gmail.com>
2018-06-27 00:44:11 +0530

    WIP

--- src/jsonResponseServer.js
+++ src/jsonResponseServer.js
@@ -4,6 +4,12 @@
 
 // const http = require('http');
 
-const server = () => {};
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application: json' });
+  res.write(JSON.stringify({ data: 'Pesto Bootcamp!' }));
+  res.end();
+}).listen(3000);
 
 module.exports = server;
