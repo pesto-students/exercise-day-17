026b331c9778d74ac45759cd92cb462a1380812e
Kundan Singh <kundan.singh979@gmail.com>
2018-06-26 23:33:42 +0530

    WIP: HTTP-Server

--- src/httpServer.js
+++ src/httpServer.js
@@ -1,7 +1,15 @@
 // (*) Create a server using http module. Check out the test cases for details
 
-// const http = require('http');
+const http = require('http');
 
-const server = () => {};
+const server = http.createServer((request, response) => {
+  response.write('Response from server!');
+  response.end();
+});
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
 
 module.exports = server;
