42792f82c599fdb2aa08361389483f7b4115fb25
adityad85 <aditya85d@gmail.com>
2018-06-27 04:42:14 +0530

    WIP

--- src/bitcoinTicker.js
+++ src/bitcoinTicker.js
@@ -1,9 +1,22 @@
+// import fetch from 'node-fetch';
 /* (*)
   Make a get request to crypto price ticker api and show the top 10 ranked
   currencies.
 
   You can use this api: https://api.coinmarketcap.com/v2/ticker/
 */
-// const http = require('http');
+const http = require('https');
 
-module.exports = () => {};
+function bitcoinTicker() {
+  http.get('https://api.coinmarketcap.com/v2/ticker/', (res) => {
+    let data = '';
+    // const count = 0;
+    res.on('data', (resp) => {
+      data += resp;
+    });
+    res.on('end', () => {
+      console.log(JSON.parse(data));
+    });
+  });
+}
+module.exports = bitcoinTicker;
