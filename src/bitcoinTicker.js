10a2e4b7a3c7c0e67f18c511008a08841cc39e2d
adityad85 <aditya85d@gmail.com>
2018-06-27 04:59:44 +0530

    WIP

--- src/bitcoinTicker.js
+++ src/bitcoinTicker.js
@@ -1,22 +1,26 @@
-// import fetch from 'node-fetch';
+const fetch = require('node-fetch');
+// Don't know why import isn't working here.
 /* (*)
   Make a get request to crypto price ticker api and show the top 10 ranked
   currencies.
 
   You can use this api: https://api.coinmarketcap.com/v2/ticker/
 */
-const http = require('https');
+// const http = require('https');
 
 function bitcoinTicker() {
-  http.get('https://api.coinmarketcap.com/v2/ticker/', (res) => {
-    let data = '';
-    // const count = 0;
-    res.on('data', (resp) => {
-      data += resp;
-    });
-    res.on('end', () => {
-      console.log(JSON.parse(data));
-    });
-  });
+  // http.get('https://api.coinmarketcap.com/v2/ticker/', (res) => {
+  //   let data = '';
+  //   // const count = 0;
+  //   res.on('data', (resp) => {
+  //     data += resp;
+  //   });
+  //   res.on('end', () => {
+  //     console.log(JSON.parse(data));
+  //   });
+  // });
+  return fetch('https://api.coinmarketcap.com/v2/ticker/')
+    .then(res => res.json());
+  // .then(body => body);
 }
 module.exports = bitcoinTicker;
