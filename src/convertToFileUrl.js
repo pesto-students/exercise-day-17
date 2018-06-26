ca81ea033c6f8f86c51c517dd1ca1ab31745e40a
Nikhil Arora <narora200@gmail.com>
2018-06-27 05:03:46 +0530

    WIP

--- src/convertToFileUrl.js
+++ src/convertToFileUrl.js
@@ -2,6 +2,13 @@
 * Convert a path to a file url: image.jpg → file:///Users/pesto/image.jpg
 */
 
-// const path = require('path')
+const path = require('path');
 
-module.exports = () => {};
+
+const convertToFileUrl = (pathname) => {
+  console.log(path.resolve(pathname));
+};
+
+convertToFileUrl('/Users/pesto/dev/te^st.jpg');
+
+module.exports = convertToFileUrl;
