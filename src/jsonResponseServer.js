92889622e6b65090d3511057acacfdc575afb884
Kundan Singh <kundan.singh979@gmail.com>
2018-06-27 00:20:32 +0530

    WIP: JSON Response Solution

--- src/jsonResponseServer.js
+++ src/jsonResponseServer.js
@@ -2,8 +2,19 @@
   Create a server using the http module. Respond with Content-Type = application/json
 */
 
-// const http = require('http');
+const http = require('http');
 
-const server = () => {};
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+
+  // response.write();
+  const responseMessage = {
+    data: 'Pesto Bootcamp!',
+  };
+  response.write(JSON.stringify(responseMessage));
+  response.end();
+});
 
 module.exports = server;
