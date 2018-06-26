589e5dab5828f500f384e29b7b46b2e6e9f9bdff
Kundan Singh <kundan.singh979@gmail.com>
2018-06-27 05:07:50 +0530

    WIP: CSVtoJSON Solution

--- src/csvToJson.js
+++ src/csvToJson.js
@@ -3,6 +3,26 @@
   formatted JSON object.
 */
 
+const fs = require('fs');
 // const path = require('path');
 
-module.exports = () => {};
+const csvToJson = (filepath) => {
+  let fileData = fs.readFileSync(filepath);
+  fileData = fileData.toString().split(/\r?\n/);
+  // fileData.map(line => console.log(line));
+
+  return fileData.reduce((acc, curr) => {
+    const currArray = curr.split(',');
+    if (currArray[0] === 'name') {
+      return [];
+    }
+    return [...acc, {
+      name: currArray[0],
+      superheroname: currArray[1],
+      power: currArray[2],
+    }];
+  }, []);
+};
+
+
+module.exports = csvToJson;
