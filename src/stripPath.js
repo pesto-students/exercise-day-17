da180ebd88a0c65fac0c0af83cf6672741f9e285
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-03 03:51:48 +0530

    Fixed stripPath

--- src/stripPath.js
+++ src/stripPath.js
@@ -10,4 +10,19 @@
     - path.normalize();
 */
 
-module.exports = () => {};
+const path = require('path');
+
+module.exports = (origPath, toStrip) => {
+  const pathOrig = path.normalize(origPath).split('/');
+  if (toStrip === null) return pathOrig.join('/');
+
+  const pathToStrip = path.normalize(toStrip).split('/');
+
+  let idx = 0;
+  for (let i = 0; i < pathToStrip.length; i += 1) {
+    if (pathOrig[i] === pathToStrip[i]) idx += 1;
+    else break;
+  }
+
+  return pathOrig.slice(idx).join('/');
+};
