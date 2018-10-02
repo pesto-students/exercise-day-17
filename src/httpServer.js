098a6622e8bb9a18e51b547e01e020a2d2200df6
vipulrawat <vipulrawat.imi@live.in>
2018-10-03 03:07:03 +0530

    [WIP]

--- src/httpServer.js
+++ src/httpServer.js
@@ -1,7 +1,16 @@
 // (*) Create a server using http module. Check out the test cases for details
 
-// const http = require('http');
+const http = require('http');
 
-const server = () => {};
+const server = () => {
+  http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    if (req.url === '/') {
+      res.write('Pesto Bootcamp!');
+    }
+    res.end();
+  });
+  server.listen(3000);
+};
 
 module.exports = server;
