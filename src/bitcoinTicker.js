d1b6c45ac8d83381b10ddac8b41e20e530f36274
vipulrawat <vipulrawat.imi@live.in>
2018-10-03 03:34:26 +0530

    [WIP]

--- src/bitcoinTicker.js
+++ src/bitcoinTicker.js
@@ -4,6 +4,25 @@
 
   You can use this api: https://api.coinmarketcap.com/v2/ticker/
 */
-// const http = require('http');
+const http = require('http');
 
-module.exports = () => {};
+module.exports = () => {
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    http.get('http://nodejs.org/dist/index.json', (resp) => {
+      let rawData = '';
+      resp.on('data', (chunk) => { rawData += chunk; });
+      resp.on('end', () => {
+        const parsedData = JSON.parse(rawData);
+        const x = Object.values(parsedData.data);
+        const mainArr = [];
+        for (let i = 0; i < 10; i += 1) {
+          mainArr.push(x[i].name);
+        }
+        return mainArr;
+      });
+    });
+  });
+  server.listen(3000);
+};
