e7f427b42b77c1df5a17d40768df3e5695484989
Bharani <bharanidharan.nb@gmail.com>
2018-10-03 04:07:47 +0530

    csvToJson

--- src/csvToJson.js
+++ src/csvToJson.js
@@ -3,6 +3,24 @@
   formatted JSON object.
 */
 
-// const path = require('path');
+const fs = require('fs');
 
-module.exports = () => {};
+module.exports = fileName => new Promise((resolve) => {
+  const fileData = fs.readFileSync(fileName);
+  const [csvHeader, ...csvData] = fileData.toString().split('\n');
+
+  const keys = csvHeader.split(',');
+
+  const obj = csvData.map((csv) => {
+    const record = {};
+    const values = csv.split(',');
+
+    keys.forEach((key, index) => {
+      record[key] = values[index];
+    });
+
+    return record;
+  });
+
+  resolve(obj);
+});
