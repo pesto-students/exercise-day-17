7073c6d4caa723fd0e6f5bf20c331ace18bb0810
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-03 03:42:45 +0530

    Fixed parseJson

--- src/parseJson.js
+++ src/parseJson.js
@@ -3,4 +3,23 @@
   Refer to the test cases of parseJson for the particular errors.
 */
 
-module.exports = () => {};
+class ParseError extends Error {
+  constructor(msg, file = '') {
+    super(msg);
+    this.msg = msg;
+
+    this.message = `${this.msg} in ${file}`;
+  }
+
+  set fileName(file) {
+    this.message = `${this.msg} in ${file}`;
+  }
+}
+
+module.exports = (jsonStr, file) => {
+  try {
+    return JSON.parse(jsonStr);
+  } catch (e) {
+    throw new ParseError(e.message, file);
+  }
+};
