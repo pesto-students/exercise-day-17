4e73a62e3e5198739bfa92f80cc50b9bf2f8368b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-03 00:11:11 +0530

    Fixed jsonResponseServer

--- src/jsonResponseServer.js
+++ src/jsonResponseServer.js
@@ -2,8 +2,12 @@
   Create a server using the http module. Respond with Content-Type = application/json
 */
 
-// const http = require('http');
+const http = require('http');
 
-const server = () => {};
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ data: 'Pesto Bootcamp!' }));
+});
 
 module.exports = server;
